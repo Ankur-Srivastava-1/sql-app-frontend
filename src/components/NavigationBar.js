9e6414bba59eb3865a086bc08659416acd661af0
Ankur Srivastava <59230660+Ankur-Srivastava-1@users.noreply.github.com>
2022-05-29 13:47:55 +0530

    Updating href
    Now points to required repo

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -9,7 +9,7 @@ function NavigationBar() {
             <Navbar.Toggle />
             <Navbar.Collapse className="justify-content-end">
                 <Navbar.Text>
-                Github Repo Link: <a href="#login">Here</a>
+                Github Repo Link: <a href="https://github.com/Ankur-Srivastava-1/sql-app-frontend">Here</a>
                 </Navbar.Text>
             </Navbar.Collapse>
           </Container>
